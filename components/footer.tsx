c13afd9dd7edd912680e227bd779c787e93aaad7
enriquepina <enrique.pina@sowifyglobalsolutions.com>
2024-07-21 18:17:10 +0200

    feat: add footer and app metadata

---
+++ components/footer.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils";
+import { LinkPreview } from "./ui/link-preview";
+
+export default function Footer({ classname }: { classname?: string }) {
+  return (
+    <footer
+      className={cn(
+        "text-xs invisible md:visible text-foreground/50",
+        classname
+      )}
+    >
+      <span>
+        Created by Enrique Pina. UI components powered by{" "}
+        <LinkPreview
+          url="https://ui.shadcn.com/"
+          isStatic={true}
+          imageSrc="shadcn.webp"
+          className="underline text-foreground/50"
+        >
+          Shadcn/UI
+        </LinkPreview>{" "}
+        and{" "}
+        <LinkPreview
+          url="https://ui.aceternity.com/"
+          isStatic={true}
+          imageSrc="aceternity.webp"
+          className="underline text-foreground/50"
+        >
+          Aceternity UI
+        </LinkPreview>{" "}
+      </span>
+    </footer>
+  );
+}
